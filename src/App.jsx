f5f77aa6f2c2e49a8925923d21ccd3cc8bff7dac
Adoosko <adoosdeveloper@gmail.com>
2025-06-25 16:10:14 +0200

    lenis added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
@@ -7,6 +9,17 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <NavBar />
